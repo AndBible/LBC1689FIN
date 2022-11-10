a10918a971ba286b4f804f08dd94366315bb6333
Tuomas Airaksinen <tuomas.airaksinen@vrt.fi>
2022-11-10 08:41:40 +0200

    Update zip & conf

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 xml2gbs LBCF1689FIN.xml modules/genbook/rawgenbook/LBCF1689FIN/LBCF1689FIN
 zip LBCF1689FIN.zip -r modules/ mods.d/
+cp LBCF1689FIN.zip ../and-bible/modules/andbible/zip/
+cp mods.d/LBCF1689FIN.conf ../and-bible/modules/andbible/mods.d/
